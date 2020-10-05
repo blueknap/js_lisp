4d0f697388e8c58b5f260cde68a35ce7640f41b5
Ali krgn <ali.sancopanco@gmail.com>
2020-10-05 12:57:36 +0200

    init

--- main.js
+++ main.js
@@ -2,155 +2,287 @@
 // Outputs a list of tokens in that string
 // A Token is an indivisible syntactic unit
 // Tokens are seperated by whitespace(spaces, tabs, newlines)
-function tokenize(chars) {
-  return chars.replace(/\(/g, " ( ").replace(/\)/g, " ) ").trim().split(/\s+/);
+class Tokenizer {
+  constructor(chars) {
+    this.chars = chars;
+  }
+
+  tokenize() {
+    return this.chars
+      .replace(/\(/g, " ( ")
+      .replace(/\)/g, " ) ")
+      .trim()
+      .split(/\s+/);
+  }
 }
 
 // A parser structures the tokens according to the program structure,
 // producing a parse tree that encodes the structure of the input program (AST)
 // Paranthesis provide the program structure, so are removed from the parse tree
-function parse(program) {
-  return readFromTokens(tokenize(program));
-}
-
-function readFromTokens(tokens) {
-  let L = [];
-  console.log("tokens", tokens);
-  if (tokens.length === 0) {
-    throw new SyntaxError("unexpected EOF");
+class Parser {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.current = 0;
   }
 
-  let token = tokens.shift();
-
-  if (token === "(") {
-    while (tokens[0] !== ")") {
-      L.push(readFromTokens(tokens));
+  //program -> listExp *
+  parse() {
+    console.log("tokens", this.tokens);
+    let list = [];
+    while (this.hasMoreTokens()) {
+      // console.log("list", this.listExp());
+      list.push(this.listExp());
     }
-    console.log("L", L);
-    tokens.shift();
-    return L;
-  } else if (token === ")") {
-    throw new SyntaxError("Unexpected");
-  } else {
-    return atom(token);
-  }
-}
-
-function atom(token) {
-  let numVal = Number(token);
-  if (numVal) {
-    return numVal;
-  }
-  return token;
-}
-
-//
-// Parser
-//
-
-function Parser(tokens) {
-  this.tokens = tokens;
-}
-
-function readFromTokens(tokens) {
-  let L = [];
-  console.log("tokens", tokens);
-  if (tokens.length === 0) {
-    throw new SyntaxError("unexpected EOF");
+    return list;
   }
 
-  let token = tokens.shift();
-
-  if (token === "(") {
-    while (tokens[0] !== ")") {
-      L.push(readFromTokens(tokens));
+  // listExp -> '(' s_expression * ')'
+  listExp() {
+    this.consume("(");
+    let list = [];
+    while (this.peek() !== ")") {
+      this.consume(")");
+      list.push(this.sExpression());
     }
-    console.log("L", L);
-    tokens.shift();
-    return L;
-  } else if (token === ")") {
-    throw new SyntaxError("Unexpected");
-  } else {
-    return atom(token);
+    this.consume(")");
+    return list;
+  }
+
+  // s_expression ->  atom | list
+  sExpression() {
+    if (this.check("(")) {
+      return this.listExp();
+    } else {
+      return this.atom();
+    }
+  }
+
+  // atom -> number | string
+  atom(token) {
+    token = this.advance();
+    let numVal = Number(token);
+    if (numVal) {
+      return numVal;
+    }
+    return token;
+  }
+
+  consume(tokeType) {
+    if (this.check(tokeType)) {
+      return this.advance();
+    }
+  }
+
+  check(token) {
+    if (!this.hasMoreTokens()) {
+      return false;
+    }
+    return this.peek() === token;
+  }
+
+  peek(ll = 0) {
+    return this.tokens[this.current + ll];
+  }
+
+  advance() {
+    if (this.hasMoreTokens()) {
+      this.current++;
+    }
+    return this.tokens[this.current - 1];
+  }
+
+  hasMoreTokens() {
+    return this.current < this.tokens.length - 1;
   }
 }
 
-// <application>  ::=  (<expression>+)
-// <expression>   ::=  <variable> | <literal>
-// <variable>     ::=  [symbol]
-// <literal>      ::=  [number] | [character] | [string] | #t | #f
-Parser.prototype.parse = function () {
-  function application() {}
-  return appication();
-};
-
-//
-// Environment
-//
-
-function Environment(encloding = null) {
-  this.enclosing = enclosing;
-  // Store bindings
-  this.values = {};
-}
-
-Environment.prototype.define = function (name, value) {
-  this.values[name] = value;
-};
-
-//
-// Interpreter
-//
-
-function Interpreter() {
-  this.globals = new Environment();
-  this.environment = this.globals;
-
-  function addNativeFuntions() {
-    this.globals.define("+", function (a, b) {
-      a + b;
-    });
-    this.globals.define("*", function (a, b) {
-      a * b;
-    });
+// Handle variable look up
+class Environment {
+  constructor(params = [], args = [], enclosing = null) {
+    this.env = {};
+    params.forEach((param, i) => this.define(param, args[i]));
+    this.enclosing = enclosing;
+    this.interpreter = null;
   }
-  addNativeFuntions();
+
+  define(name, value) {
+    this.env[name] = value;
+  }
+
+  // Get the val from the innermost env where var(name) appears
+  get(name) {
+    console.log("look for", name, "in", this.env);
+
+    let lookupVal = this.env[name];
+    if (lookupVal) {
+      return lookupVal;
+    }
+
+    if (this.enclosing === null) {
+      throw new Error(`Could not found symbol ${name}`);
+    }
+
+    return this.enclosing.get(name);
+  }
 }
 
-Interpreter.prototype.interpret = function (expressions) {
-  expressions.forEach(function (expression) {
-    this.evaluate(expression);
-  });
-};
+// A user defined Scheme procedure
+class Procedure {
+  constructor(params, body, definitionEnv) {
+    this.params = params;
+    this.body = body;
+    this.definitionEnv = definitionEnv;
+    this.interpreter = this.definitionEnv.interpreter;
+  }
 
-Interpreter.prototype.evaluate = function (expression) {
-  if (typeof expression === "number") {
+  call(...args) {
+    console.log("Proc call with", args);
+    this._setEvalutionEnv(...args);
+    return this.interpreter.eval(this.body);
+  }
+
+  // Create new env/context for the evaluation -- defintion env as the parent
+  _setEvalutionEnv(args) {
+    console.log("args", args);
+    this.interpreter.env = new Environment(
+      this.params,
+      args,
+      this.definitionEnv
+    );
+  }
+}
+
+class Interpreter {
+  constructor() {
+    this.globals = new Environment();
+    this._addNativeFunctions();
+    this.env = this.globals;
+    this.env.interpreter = this;
+  }
+
+  interpret(expressions) {
+    return expressions.map((expression) => this.eval(expression)).pop();
+  }
+
+  eval(expression) {
+    console.log("expression", expression);
+    // Variable reference
+    if (typeof expression === "string") {
+      return this.symbol(expression);
+    }
+
+    if (typeof expression === "number") {
+      return this.number(expression);
+    }
+
+    let [op, ...args] = expression;
+
+    // conditional
+    if (op === "if") {
+      let [test, if_body, else_body] = args;
+      let exp = this.eval(test) ? if_body : else_body;
+      return this.eval(exp);
+    }
+
+    // definition
+    if (op === "define") {
+      let [symbol, exp] = args;
+      return this.env.define(symbol, this.eval(exp));
+    }
+
+    // procedure call
+    if (op === "lambda") {
+      console.log("Eval lambda");
+      let [params, body] = args;
+      return new Procedure(params, body, this.env);
+    }
+
+    return this.procCall(expression);
+  }
+
+  symbol(expression) {
+    return this.env.get(expression);
+  }
+
+  number(expression) {
     return expression;
-  } else {
-    // syntax:  (<operator> <operand1> ...)
-    // Procedure calls, e.g. (+ 3 4)
-    let operatorExpr = expression[0];
-    let operandExprs = expression.slice(1);
-    let operator = this.evaluate(operatorExpr);
-    let operands = operandExprs.forEach(function (operandExpr) {
-      this.evaluate(operandExpr);
-    });
-    return operator.apply(operands);
   }
-};
+
+  // (proc args...)
+  // Evaluate proc and all the args
+  // apply proc to the args values
+  procCall(expression) {
+    console.log("procCall", expression);
+    let procObj = this.eval(expression[0]);
+    let args = [];
+    expression.slice(1).forEach((arg) => {
+      args.push(this.eval(arg));
+    });
+    console.log("procObj", procObj);
+    if (typeof procObj === "function") {
+      return procObj.apply(null, args);
+    }
+    return procObj.call(args);
+  }
+
+  _addNativeFunctions() {
+    this.globals.define("+", (a, b) => a + b);
+
+    this.globals.define("-", (a, b) => a - b);
+
+    this.globals.define("*", (a, b) => a * b);
+
+    this.globals.define("<=", (a, b) => a <= b);
+
+    this.globals.define("list", function (...args) {
+      return args;
+    });
+
+    this.globals.define("first", function (list) {
+      return list[0];
+    });
+
+    this.globals.define("begin", function (...args) {
+      return args[args.length - 1];
+    });
+
+    this.globals.define("pi", Math.PI);
+  }
+}
 
 function run(source) {
-  let tokens = tokenize(source);
+  let tokens = new Tokenizer(source).tokenize(source);
   let parser = new Parser(tokens);
   let ast = parser.parse(tokens);
+  console.log("AST", ast);
   let interpreter = new Interpreter();
-  interpreter.interpret(ast);
+  let result = interpreter.interpret(ast);
+  console.log("RES", result);
+  return result;
 }
 
-// program = "(begin (define r 10) (* pi (* r r)))";
-program = "(+ 1 2)";
-program = "(+ 1 (* 2 3))";
+function test(program, output) {
+  if (JSON.stringify(run(program)) === JSON.stringify(output)) {
+    console.log("CORRECT");
+  } else {
+    console.log("FAILED");
+  }
+}
 
-run(program);
+function testEval() {
+  // program = "(+ 1 2)";
+  // test(program, 3);
+  // program = "(+ 1 (* 2 3))";
+  // test(program, 7);
+  // program = "(first (list 1 (+ 2 3) 9))";
+  // test(program, 1);
+  // program = "(begin (define r 10) (* pi (* r r)))";
+  // test(program, 100 * Math.PI);
+  program = "(define circle-area (lambda (r) (* pi (* r r)))) (circle-area 3)";
+  program = "(define twice (lambda (x) (* 2 x))) (twice 5)";
+  program =
+    "(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1)))))) (fact 10)";
+  test(program, 3628800);
+}
 
-console.log(ast);
+testEval();
